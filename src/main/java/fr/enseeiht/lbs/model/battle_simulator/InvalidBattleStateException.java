4222c8d45dc54ff6a098ee5253d351e4c47e1d65
oxabz <legmatt0@gmail.com>
2021-06-08 16:37:37 +0200

    Fixing the bug if there's no unit in the battle

---
+++ src/main/java/fr/enseeiht/lbs/model/battle_simulator/InvalidBattleStateException.java
@@ -0,0 +1,7 @@
+package main.java.fr.enseeiht.lbs.model.battle_simulator;
+
+public class InvalidBattleStateException extends Exception {
+    public InvalidBattleStateException(String message) {
+        super(message);
+    }
+}
