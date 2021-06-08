c0e6d6ada356583b71f75ea8f55ebeefaafd7e22
Thomas Nadal <Thomas Nadal Elizalde>
2021-06-08 09:48:56 +0200

    samurai behaviour works

---
+++ src/main/java/fr/enseeiht/lbs/model/game_object/unit/action/SamuraiAttack.java
@@ -0,0 +1,28 @@
+package main.java.fr.enseeiht.lbs.model.game_object.unit.action;
+
+import main.java.fr.enseeiht.lbs.model.game_object.Statistic;
+import main.java.fr.enseeiht.lbs.model.game_object.unit.Unit;
+import main.java.fr.enseeiht.lbs.model.game_object.unit.buff.FireDebuff;
+
+public class SamuraiAttack extends AttackAction {
+
+    public SamuraiAttack(Unit attaquant) {
+        super(attaquant);
+    }
+
+    @Override
+    public void execute(long deltaTime) {
+        if (target == null||attaquant==null) {
+            return;
+        }
+
+        if (attaquant.attackSuccess()) {
+            target.receiveDamage(attaquant.getStats().getStatisticValue(Statistic.DAMAGE));
+
+            FireDebuff fireDebuff = new FireDebuff(10);
+            if (!target.hasBuff(fireDebuff)) {
+                target.addBuffs(fireDebuff);
+            }
+        }
+    }
+}
