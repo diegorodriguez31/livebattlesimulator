be95978d5acfca5e0bdf4d41711303ea787f01be
Thomas Nadal <Thomas Nadal Elizalde>
2021-06-10 18:28:41 +0200

    last little changes

--- src/main/java/fr/enseeiht/lbs/model/game/object/unit/visitor/BuffVisitor.java
+++ src/main/java/fr/enseeiht/lbs/model/game/object/unit/visitor/BuffVisitor.java
@@ -3,12 +3,8 @@ package main.java.fr.enseeiht.lbs.model.game.object.unit.visitor;
 import main.java.fr.enseeiht.lbs.model.game.object.unit.buff.*;
 
 /**
-<<<<<<< HEAD:src/main/java/fr/enseeiht/lbs/model/game_object/unit/visitor/BuffVisitor.java
- * Spécifie les comportements à définir en fonction de chaque type de Buff
-=======
  * Spécifie les comportments à définir
  * en fonction de chaque type de Buff appliqué à une unité
->>>>>>> origin/develop:src/main/java/fr/enseeiht/lbs/model/game/object/unit/visitor/BuffVisitor.java
  */
 public interface BuffVisitor {
     void visit(FireDebuff buff);
